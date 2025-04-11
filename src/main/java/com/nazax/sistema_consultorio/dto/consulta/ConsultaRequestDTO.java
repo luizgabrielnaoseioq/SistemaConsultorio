0f82d6e749912471144f05c734f98075454d7aa0
Nazax <gabrielon6689@gmail.com>
2025-04-10 23:15:17 -0300

    Metodos criados

--- src/main/java/com/nazax/sistema_consultorio/dto/consulta/ConsultaRequestDTO.java
+++ src/main/java/com/nazax/sistema_consultorio/dto/consulta/ConsultaRequestDTO.java
@@ -1,15 +1,56 @@
 package com.nazax.sistema_consultorio.dto.consulta;
 
-import lombok.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ConsultaRequestDTO {
     private Long pacienteId;
     private Long medicoId;
     private LocalDateTime dataHora;
     private String observacoes;
+
+    public ConsultaRequestDTO() {
+
+    }
+
+    public ConsultaRequestDTO(Long pacienteId, Long medicoId, LocalDateTime dataHora, String observacoes) {
+        this.pacienteId = pacienteId;
+        this.medicoId = medicoId;
+        this.dataHora = dataHora;
+        this.observacoes = observacoes;
+    }
+
+    public Long getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(Long pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public Long getMedicoId() {
+        return medicoId;
+    }
+
+    public void setMedicoId(Long medicoId) {
+        this.medicoId = medicoId;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
 }
--- src/main/java/com/nazax/sistema_consultorio/dto/consulta/ConsultaResponseDTO.java
+++ src/main/java/com/nazax/sistema_consultorio/dto/consulta/ConsultaResponseDTO.java
@@ -1,19 +1,67 @@
 package com.nazax.sistema_consultorio.dto.consulta;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ConsultaResponseDTO {
     private Long id;
     private String nomePaciente;
     private String nomeMedico;
     private LocalDateTime dataHora;
     private String observacoes;
+
+    public ConsultaResponseDTO() {
+
+    }
+
+    public ConsultaResponseDTO(Long id, String nomePaciente, String nomeMedico, LocalDateTime dataHora, String observacoes) {
+        this.id = id;
+        this.nomePaciente = nomePaciente;
+        this.nomeMedico = nomeMedico;
+        this.dataHora = dataHora;
+        this.observacoes = observacoes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+
+    public void setNomePaciente(String nomePaciente) {
+        this.nomePaciente = nomePaciente;
+    }
+
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+
+    public void setNomeMedico(String nomeMedico) {
+        this.nomeMedico = nomeMedico;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
 }
 
