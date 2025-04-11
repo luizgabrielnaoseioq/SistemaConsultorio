742585803d3aec857e99a578e75610eabb04448a
Nazax <gabrielon6689@gmail.com>
2025-04-10 23:13:22 -0300

    Criação de contrutores e  getters and setters

--- src/main/java/com/nazax/sistema_consultorio/model/Consulta.java
+++ src/main/java/com/nazax/sistema_consultorio/model/Consulta.java
@@ -5,11 +5,12 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Entity
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table(name = "consulta")
 public class Consulta {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,5 +23,57 @@ public class Consulta {
 
     private LocalDateTime dataHora;
     private String observacoes;
+
+    public Consulta() {
+
+    }
+
+    public Consulta(Long id, Paciente paciente, Medico medico, LocalDateTime dataHora, String observacoes) {
+        this.id = id;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.dataHora = dataHora;
+        this.observacoes = observacoes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
 }
 
